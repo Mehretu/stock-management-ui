d04f2a3950351c21889def885cbb655f35a4c7d2
Mehretu <mehertu.abreham@gmail.com>
2024-04-18 10:07:20 +0300

    fetching change

--- app/(back-office)/inventory-dashboard/inventory/adjustments/new/page.jsx
+++ app/(back-office)/inventory-dashboard/inventory/adjustments/new/page.jsx
@@ -1,18 +1,27 @@
+"use client"
 import AdjustmentForm from '@/components/dashboard/AdjustmentForm'
 import { getData } from '@/lib/getData'
+import { useEffect, useState } from 'react'
 
 
-export default async function NewAdjustments() {
+export default function NewAdjustments() {
+const [items,setItems] = useState([]);
+const [warehouses,setWarehouses] = useState([])
+const [suppliers,setSuppliers] = useState([])
+
+useEffect(() => {
+  async function fetchItems(){
+    const itemsData =  await getData("items")
+    const warehousesData = await getData("warehouse")
+    const suppliersData = await getData("supplier")
+    setItems(itemsData)
+    setWarehouses(warehousesData)
+    setSuppliers(suppliersData)
+
+  }
+  fetchItems()
+},[])
   
-const itemsData =  getData("items")
-const warehousesData =  getData("warehouse")
-const suppliersData = getData("supplier")
-
-const [items,warehouses,suppliers] = await Promise.all([
-    itemsData,
-    warehousesData,
-    suppliersData
-  ]);
 
   return (
     <AdjustmentForm items={items} warehouses={warehouses} suppliers={suppliers}/>
