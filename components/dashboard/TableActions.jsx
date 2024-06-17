0680c146368ad839565c3c80378c74a980f51db9
Mehretu <mehertu.abreham@gmail.com>
2024-06-17 12:04:09 +0300

    responsive table actions

--- components/dashboard/TableActions.jsx
+++ components/dashboard/TableActions.jsx
@@ -142,218 +142,162 @@ export default function TableActions(
 
  
   return (
-    <div className="flex flex-col mx-1 mt-2 md:flex-row md:justify-between py-6 px-6 bg-slate-200 rounded-lg md:items-center gap-2">
-      <div className='flex items-center justify-between w-full md:w-auto mb-0'>
-      <button 
+    <div className="flex flex-col mx-1 mt-2 md:flex-row md:justify-between py-4 px-4 bg-slate-200 rounded-lg md:items-center gap-2">
+  <div className='flex items-center justify-between w-full md:w-auto mb-0'>
+    <button 
       onClick={()=> {onPrint();}}
-      className="mx-2 my-0 flex items-center gap-1 text-white bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-md shadow-blue-500/50  font-small rounded-md text-xs px-2 py-1 text-center ">
-      <Download className='w-4 '/>
-      <span className='hidden md:block' >Export</span>
-      </button>
-    {/* {search} */}
-        <div className="relative flex-grow md:flex-grow-0 md:ml-4">
-          <label htmlFor="table-search" className="sr-only">Search</label>
-          <div className="relative">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none ">
-                <Search className="w-4 h-4 text-gray-500 text-xs"/>
-              </div>
-              <input 
-                type="text" 
-                id="table-search" 
-                className="block py-2 pl-10 pr-3 text-xs font-medium text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full" 
-                placeholder={`Search for ${title}`}
-                value={searchQuery}
-                onChange={(e) => onSearch(e.target.value)}
-                />
-          </div>
+      className="mx-1 my-0 flex items-center gap-1 text-white bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-md shadow-blue-500/50 font-small rounded-md text-xs px-2 py-1 text-center">
+      <Download className='w-4'/>
+      <span className='hidden md:block'>Export</span>
+    </button>
+    <div className="relative flex-grow md:flex-grow-0 md:ml-2">
+      <label htmlFor="table-search" className="sr-only">Search</label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <Search className="w-4 h-4 text-gray-500 text-xs"/>
+        </div>
+        <input 
+          type="text" 
+          id="table-search" 
+          className="block py-2 pl-10 pr-3 text-xs font-medium text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full" 
+          placeholder={`Search for ${title}`}
+          value={searchQuery}
+          onChange={(e) => onSearch(e.target.value)}
+        />
       </div>
-    {/* Filter Button for Mobile */}
-      <button
-            onClick={handleToggleFilters}
-            className="m-2 flex items-center gap-2 text-gray-600 hover:text-gray-900 focus:outline-none md:hidden"
-          >
-            <Filter className='w-4 h-4'/>
-          </button>
-
+    </div>
+    <button
+      onClick={handleToggleFilters}
+      className="m-2 flex items-center gap-1 text-gray-600 hover:text-gray-900 focus:outline-none md:hidden">
+      <Filter className='w-4 h-4'/>
+    </button>
   </div>
-      <div className={`mt-0 flex md:mt-0 md:flex md:gap-1 ${showFilters ? '' : 'hidden md:flex'} items-center`}>
-        {
-          (isBalance)?"":
-            <span className='text-xs font-medium text-gray-600'>Filter By:</span>
-
-          
-        }
-      {
-        isItem? 
-          (
-            <select
-        value={selectedCategory}
-        onChange={handleCategoryChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mr-1  "
-      >
-        <option value="" >Category</option>
-        {categories.map((category) => (
-          <option 
-                key={category.id} 
-                value={category.id} 
-                className="bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {category.title}
-          </option>
-        ))}
-      </select>
-          ):null
-        
-      }
-      {
-        isItem ? (
-          <select
-        value={selectedShop}
-        onChange={handleShopChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Shop</option>
-        {shops.map((shop) => (
-          <option 
-                key={shop.id} 
-                value={shop.id} 
-                className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {shop.title}
-          </option>
-        ))}
-      </select>
-        ):null
-      }
-      {
-        isItem? (
-          <select
-        value={selectedWarehouse}
-        onChange={handleWarehouseChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Store</option>
-        {warehouses.map((warehouse) => (
-          <option 
-                key={warehouse.id} 
-                value={warehouse.id} 
-                className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {warehouse.title}
-          </option>
-        ))}
-      </select>
-        ): null
-      }
-      {
-        isItem ? (
-          <select
-        value={selectedStatus}
-        onChange={handleStatusChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Status</option>
-        {statuses.map((status) => (
-          <option 
-                key={status.id} 
-                value={status.id} 
-                className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {status.title}
-          </option>
-        ))}
-      </select>
-        ):null
-      }
-      {
-        isSales?(
-          <select
-        value={selectedPaymentStatus}
-        onChange={handlePaymentStatusChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Payment Status</option>
-        {paymentStatus.map((status) => (
-          <option 
-                key={status.id} 
-                value={status.id} 
-                className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {status.title}
-          </option>
-        ))}
-      </select>
-        ):null
-      }
-      {
-        isSales?(
-          <select
-        value={selectedOrderStatus}
-        onChange={handleOrderStatusChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Order Status</option>
-        {orderStatus.map((status) => (
-          <option 
-                key={status.id} 
-                value={status.id} 
-                className=" bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {status.title}
-          </option>
-        ))}
-      </select>
-        ):null
-      }
-      
-      {
-        isPurchase?(
-          <select
-        value={selectedPurchaseStatus}
-        onChange={handlePurchaseStatusChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Purchase Status</option>
-        {purchaseStatus.map((status) => (
-          <option 
-                key={status.id} 
-                value={status.id} 
-                className=" bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {status.title}
-          </option>
-        ))}
-      </select>
-        ):null
-      }
-            {
-        isPurchase || isSales?(
-          <select
-        value={selectedDateRange}
-        onChange={handleDateRangeChange}
-        className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-3 m-1"
-      >
-        <option value="" >Date Range</option>
-        {dateRanges.map((dateRange) => (
-          <option 
-                key={dateRange.id} 
-                value={dateRange.id} 
-                className="bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600"
-                >
-            {dateRange.label}
-          </option>
-        ))}
-      </select>
-        ):null
-      }
-     
-     
-    
-      
-        
-      </div>
-
- 
+  <div className={`mt-0 flex md:mt-0 md:flex md:gap-1 ${showFilters ? '' : 'hidden md:flex'} items-center flex-wrap`}>
+    {
+      (isBalance) ? "" : <span className='text-xs font-medium text-gray-600'>Filter By:</span>
+    }
+    {
+      isItem && (
+        <select
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Category</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id} className="bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {category.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      isItem && (
+        <select
+          value={selectedShop}
+          onChange={handleShopChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Shop</option>
+          {shops.map((shop) => (
+            <option key={shop.id} value={shop.id} className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {shop.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      isItem && (
+        <select
+          value={selectedWarehouse}
+          onChange={handleWarehouseChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Store</option>
+          {warehouses.map((warehouse) => (
+            <option key={warehouse.id} value={warehouse.id} className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {warehouse.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      isItem && (
+        <select
+          value={selectedStatus}
+          onChange={handleStatusChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Status</option>
+          {statuses.map((status) => (
+            <option key={status.id} value={status.id} className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {status.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      isSales && (
+        <select
+          value={selectedPaymentStatus}
+          onChange={handlePaymentStatusChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Payment Status</option>
+          {paymentStatus.map((status) => (
+            <option key={status.id} value={status.id} className="z-10 bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {status.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      isSales && (
+        <select
+          value={selectedOrderStatus}
+          onChange={handleOrderStatusChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Order Status</option>
+          {orderStatus.map((status) => (
+            <option key={status.id} value={status.id} className="bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {status.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      isPurchase && (
+        <select
+          value={selectedPurchaseStatus}
+          onChange={handlePurchaseStatusChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Purchase Status</option>
+          {purchaseStatus.map((status) => (
+            <option key={status.id} value={status.id} className="bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {status.title}
+            </option>
+          ))}
+        </select>
+      )
+    }
+    {
+      (isPurchase || isSales) && (
+        <select
+          value={selectedDateRange}
+          onChange={handleDateRangeChange}
+          className="bg-gray-50 border border-gray-300 text-gray-600 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2 mx-1 my-1">
+          <option value="">Date Range</option>
+          {dateRanges.map((dateRange) => (
+            <option key={dateRange.id} value={dateRange.id} className="bg-gray-100 divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 dark:divide-gray-600">
+              {dateRange.label}
+            </option>
+          ))}
+        </select>
+      )
+    }
   </div>
+</div>
+
   )
 }
